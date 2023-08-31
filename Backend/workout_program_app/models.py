056fcee76d409c9a58dec02b746e03e7688d66e1
Michael McIntosh <michaeldmc58@gmail.com>
2023-08-31 13:36:14 -0700

    added to models ?

--- Backend/workout_program_app/models.py
+++ Backend/workout_program_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from user_app.models import App_user
+# from workout_app import Workout
 
-# Create your models here.
+class Workout_Programs(models.Model):
+    program_name = models.CharField(max_length=30)
+    program_details = models.TextField()
+    program_difficulty = models.CharField(max_length=20)
+    program_duration = models.CharField(max_length=20)
+    frequency_per_week = models.PositiveBigIntegerField()
+    
+    user = models.ForeignKey(App_user, on_delete=models.CASCADE)
+    # workouts = models.ManyToManyField(Workout, related_name='parent_program', on_delete=models.CASCADE)
