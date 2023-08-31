04bc7eef3c95d3c708a5a42fab31f90973c7e421
Tristan Zimmerman <tristanzimmerman95@gmail.com>
2023-08-31 15:34:48 -0400

    Group Project: Create Exercise and Workout models

--- Backend/workout_app/models.py
+++ Backend/workout_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from user_app.models import App_user
+# from workout_program_app import Workout_program
 # Create your models here.
+
+class Workout(models.Model):
+    workout_name = models.CharField(unique=True)
+    app_user = models.ForeignKey(App_user, on_delete=models.CASCADE, related_name="workouts")
+    workout_details = models.TextField()
+    #exercises, linked to Exercise
+    #parent_program = models.ManyToManyField(Workout_Program, related_name="workouts", blank=True)
+
+    def __str__(self):
+        return f"{self.workout_name}"
