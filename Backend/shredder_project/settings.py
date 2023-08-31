b6dc6800818fa1e8e5fa0ea6247f838ba28ce8f9
Michael McIntosh <michaeldmc58@gmail.com>
2023-08-31 09:27:44 -0700

    created workout_program app

--- Backend/shredder_project/settings.py
+++ Backend/shredder_project/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'workout_program_app',
 ]
 
 ROOT_URLCONF = 'shredder_project.urls'
