93dfc6410fb03e4280ec6d4563176d6527abd0ab
Jake M <jakemarkisohn@gmail.com>
2023-09-01 12:53:01 -0400

    Urls added to survey app and project urls file

--- Backend/shredder_project/urls.py
+++ Backend/shredder_project/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/users/', include('user_app.urls')),
-    path('api/v1/workouts/', include('workout_app.urls')),
+    path("admin/", admin.site.urls),
+    path("api/v1/users/", include("user_app.urls")),
+    path("api/v1/workouts/", include("workout_app.urls")),
+    path("api/v1/survey/", include("survey_app.urls")),
 ]
