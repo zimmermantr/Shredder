f2df258ffb75a4e3bbfdfa788f7e3e7e562847bb
Marion Gordon <mariongordon3@yahoo.com>
2023-09-01 15:28:43 -0500

    finished nutrition_app

--- Backend/workout_program_app/urls.py
+++ Backend/workout_program_app/urls.py
@@ -4,5 +4,5 @@ from .views import All_programs, A_program
 urlpatterns = [
     path('', All_programs.as_view()),
     path('<int:id>/', A_program.as_view()),
-    path('<int:id>/workouts/', include('workout_app.urls'))
+    path('<int:id>/workouts/', include('workout_app.urls')),
 ]
