9a51c15f7893a6c825ce873aa0ae7825e42ce418
Michael McIntosh <michaeldmc58@gmail.com>
2023-09-06 14:34:15 -0700

    homepage update 2

---
+++ Frontend/src/pages/Home/features/JoinNow.jsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+import Button from '../../Nav/Button'
+
+const Join = styled.div`
+height: 10rem;
+background: #100e0eb2;
+color: whitesmoke;
+display: flex;
+justify-content: center;
+align-items: center;
+
+text-align: center;
+
+margin-top: 5rem;
+`
+
+const JoinTitle = styled.div`
+font-size: 2rem;
+`
+const JoinText = styled.div`
+font-size: 2rem;
+`
+const JoinLeft = styled.div`
+display: flex;
+flex-direction: column;
+padding: 4rem;
+`
+
+const JoinNow = ({title, text}) => {
+
+    return (
+        <Join>
+            <JoinLeft>
+                <JoinTitle>{title}</JoinTitle>
+                <JoinText>{text}</JoinText>
+            </JoinLeft>
+            <Button />
+        </Join>
+    )
+}
+
+export default JoinNow;
