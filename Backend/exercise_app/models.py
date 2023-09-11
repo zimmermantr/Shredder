b1f1e924d943cedc40a986f04f6689614c027cce
Jake M <jakemarkisohn@gmail.com>
2023-09-11 11:47:45 -0400

    backend work

--- Backend/exercise_app/models.py
+++ Backend/exercise_app/models.py
@@ -2,14 +2,15 @@ from django.db import models
 from django.core import validators as v
 from user_app.models import App_user
 
+
 # Create your models here.
 class Exercise(models.Model):
     exercise_name = models.CharField(max_length=50)
     force = models.CharField(max_length=10)
-    sets = models.CharField(max_length=5) #maybe char? 3-5?
-    reps = models.CharField(max_length=12) #maybe char? 8-12?
+    sets = models.CharField(max_length=5)  # maybe char? 3-5?
+    reps = models.CharField(max_length=20)  # maybe char? 8-12?
     difficulty = models.CharField(max_length=12)
-    equipment = models.CharField(max_length=20)
+    equipment = models.CharField(max_length=30)
     instructions = models.TextField(max_length=5000)
     primary_muscle = models.CharField()
     secondary_muscle = models.TextField(null=True)
@@ -19,5 +20,7 @@ class Exercise(models.Model):
 
 
 class User_Exercise(models.Model):
-    user_id = models.ForeignKey(App_user, related_name="userExercises", on_delete=models.CASCADE)
+    user_id = models.ForeignKey(
+        App_user, related_name="userExercises", on_delete=models.CASCADE
+    )
     exercise_id = models.ForeignKey(Exercise, on_delete=models.CASCADE)
